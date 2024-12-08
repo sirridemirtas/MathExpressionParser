b13170b91ef05d225bec44a249af82e970e6c000
Sirri Demirtaş <sirridemirtas@gmail.com>
2024-12-08 21:19:57 +0300

    Add token details display in TokenList

--- src/components/TokenList.tsx
+++ src/components/TokenList.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import clsx from "clsx";
-import { TokenType } from "../lib/Lexer";
+import { TokenType, Token } from "../lib/Lexer";
 
 interface TokenListProps {
   tokens: { type: TokenType; lexeme: string }[];
@@ -18,25 +18,46 @@ const TokenList: React.FC<TokenListProps> = ({ tokens, tokenColors }) => {
   }, [tokens]);
 
   return (
-    <div className={clsx("font-mono", "flex flex-row flex-wrap")}>
-      {tokens.map((token, index) => (
-        <span
-          key={index}
-          onClick={() => setSelectedToken(token)}
-          className={clsx(
-            String("text-" + tokenColors[token.type] + "-600"),
-            "hover:bg-gray-200 transition-all",
-            "px-1 cursor-pointer rounded-sm"
-          )}
-        >
-          {token.lexeme}
-        </span>
-      ))}
-      {selectedToken && (
-        <div className="w-full">
-          <pre className="text-sm p-4 rounded-lg bg-neutral-50 mt-2">
-            {JSON.stringify(selectedToken, null, 2)}
-          </pre>
+    <div>
+      <div className={clsx("font-mono", "flex flex-row flex-wrap")}>
+        {tokens.map((token, index) => (
+          <span
+            key={index}
+            onClick={() => setSelectedToken(token)}
+            className={clsx(
+              String("text-" + tokenColors[token.type] + "-600"),
+              "hover:bg-gray-200 transition-all",
+              "px-1 cursor-pointer rounded-sm"
+            )}
+          >
+            {token.lexeme}
+          </span>
+        ))}
+      </div>
+      {selectedToken && <TokenDetails token={selectedToken} />}
+    </div>
+  );
+};
+
+const TokenDetails: React.FC<{ token: Token }> = ({ token }) => {
+  return (
+    <div className="bg-neutral-50 p-4 rounded-lg mt-2 space-y-2">
+      {token.type && (
+        <div>
+          <span>Type: </span>
+          <span>{token.type}</span>
+        </div>
+      )}
+      {token.lexeme && (
+        <div>
+          <span>Lexeme: </span>
+          <span>{token.lexeme}</span>
+        </div>
+      )}
+      {token.literal && (
+        <div>
+          <span>Literal: </span>
+          <span>{token.literal}</span>
         </div>
       )}
     </div>
